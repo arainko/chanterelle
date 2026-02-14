42994e6c971a2713c79391497eec6a7727faed3f
arainko <aleksander.rainko99@gmail.com>
2026-02-14 09:17:21 +0100

    remove debug logs

--- chanterelle/src/main/scala/chanterelle/internal/PathSelector.scala
+++ chanterelle/src/main/scala/chanterelle/internal/PathSelector.scala
@@ -91,7 +91,7 @@ private[chanterelle] object PathSelector {
           Path(ident.tpe.asType, acc.toVector)
 
         case other =>
-          quotes.reflect.report.errorAndAbort(s"Matched an unexpected term: ${Printer.TreeStructure.show(other)}")
+          Logger.debug(s"Matched an unexpected term: ${Printer.TreeStructure.show(other)}")
           val pos = expr.pos
           val code = pos.sourceCode.mkString
           outer.reflect.report.errorAndAbort(s"Couldn't parse '$code' as a valid path selector", pos)
