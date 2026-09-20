e880bcf40b9590dd0f70dc0713a55ae432a2d542
arainko <aleksander.rainko99@gmail.com>
2026-09-20 17:48:40 +0200

    keep on iterating

--- chanterelle/src/main/scala/chanterelle/interop/CollectionLike.scala
+++ chanterelle/src/main/scala/chanterelle/interop/CollectionLike.scala
@@ -5,22 +5,57 @@ import scala.collection.Factory
 import scala.collection.immutable.HashMap
 import scala.collection.mutable.HashMap as MutHashMap
 import scala.collection.MapOps
+import scala.collection.immutable.SortedSet
 
-opaque type CollectionLike[Coll[_], Elem] = IterableOnceOps[Elem, Coll, ?] | CollectionLike.Interopped[Coll, Elem]
+object BuiltIn
+type BuiltIn = BuiltIn.type
 
-object CollectionLike {
+// opaque type IsColl[Coll[_]] = BuiltIn | Support.Collection[Coll]
 
-  trait Interopped[Coll[_], Elem] {
+object IsColl {
+
+  // def iterator[Coll[_]: IsColl as Coll, Elem](coll: Coll[Elem]): Iterator[Elem] = Coll match {
+  //   case BuiltIn                        => coll.asInstanceOf[IterableOnceOps[Elem, Coll, ?]].toIterator
+  //   case supp: Support.Collection[Coll] => supp.iterator(coll)
+  // }
+
+}
+
+object Support {
+
+  trait Collection[Coll[_], Elem] {
     def iterator(coll: Coll[Elem]): Iterator[Elem]
-    def factory[Elem]: Factory[Elem, Coll[Elem]]
+    def factory: Factory[Elem, Coll[Elem]]
   }
+
+  // trait Mode[F[_]] {
+  //
+  //   def traverseCollection[Coll[_]: Collection as Coll, A, B](source: Coll[A], f: A => F[B]): F[Coll[B]] = {
+  //     val iter = Coll.iterator(source)
+  //     val builder = Coll.factory[B].newBuilder
+  //     while iter.hasNext do {
+  //       ???
+  //     }
+  //
+  //     ???
+  //   }
+  // }
 
   val hashMap: MapOps[Int, Int, MutHashMap, ?] = MutHashMap(1 -> 1)
 
-  extension [Coll[_], Elem](coll: CollectionLike[Coll, Elem]) {
-    def mapped[B](f: Elem => B): Coll[B] = coll match {
-      case it: IterableOnceOps[Elem @unchecked, Coll @unchecked, ?] => it.map(f)
-      case interop: Interopped[Coll @unchecked, Elem @unchecked]    => interop.iterator(coll).map(f).to(interop.factory[B])
-    }
+  given builtInColl[Elem, Coll[a] <: IterableOnce[a]](using Fac: Factory[Elem, Coll[Elem]]): Collection[Coll, Elem] with {
+
+    def factory: Factory[Elem, Coll[Elem]] = Fac
+
+    def iterator(coll: Coll[Elem]): Iterator[Elem] = coll.iterator
   }
+
+  builtInColl[Int, SortedSet]
+
+  // extension [Coll[_], Elem](coll: CollectionLike[Coll, Elem]) {
+  //   def mapped[B](f: Elem => B): Coll[B] = coll match {
+  //     case it: IterableOnceOps[Elem @unchecked, Coll @unchecked, ?] => it.map(f)
+  //     case interop: Interopped[Coll @unchecked, Elem @unchecked]    => interop.iterator(coll).map(f).to(interop.factory[B])
+  //   }
+  // }
 }
